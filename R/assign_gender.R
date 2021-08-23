507454824284432c47fb79fee8b22b9c35f01b5a
rgiord <giordanor@rsc.org>
2021-08-23 12:30:57 +0100

    assign_gender.R: remove library

--- R/assign_gender.R
+++ R/assign_gender.R
@@ -10,14 +10,7 @@
 #'
 #'          gender - assigned gender (F/M/U)
 #' @examples
-#' gender <- assign_gender(authors, first_name)
-#' @importFrom dplyr mutate
-#' @importFrom dplyr filter
-#' @importFrom dplyr left_join
-#' @importFrom dplyr select
-#' @importFrom dplyr rename
-#' @importFrom dplyr if_else
-#' @importFrom tidyr replace_na
+#' gender <- assign_gender(authors, "first_name")
 #'
 #' @export
 
@@ -26,14 +19,13 @@ assign_gender <- function(data_df, first_name_col) {
   ## read the data frame and convert special character to ASCII to better assign 
   ## the gender. The first name need to be without accent or special character,
   ## this is why we use here the function iconv.
-  df1 <- data_df %>% 
-    mutate(FNAME = tolower(iconv({{first_name_col}}, from = "UTF-8", 
-                                 to = "ASCII//TRANSLIT"))) %>%
-    left_join(gender_names, by = c("FNAME" = "Name")) %>% 
-    mutate(UKUS_Gender = replace_na(.data$UKUS_Gender, "U")) %>%
-    mutate(gender = if_else(.data$UKUS_Gender == "Male", "M",
-                            if_else(.data$UKUS_Gender == "Female", "F",
-                                    "U"))) %>%
-    select(-.data$UKUS_Gender, -.data$FNAME)
-  df1
+  df <- data_df
+  df$Name <- tolower(iconv(df[,first_name_col], from = "UTF-8", 
+                           to = "ASCII//TRANSLIT"))
+  df <- merge(df, gender_names, by.x = "Name", by.y = "Name", all.x = TRUE)
+  df$UKUS_Gender[is.na(df$UKUS_Gender)] <- "U"
+  df$gender <- ifelse(df$UKUS_Gender == "Male", "M",
+                      ifelse(df$UKUS_Gender == "Female", "F", "U"))
+  df <- subset(df, select = -c(UKUS_Gender, Name))
+  df
 }
